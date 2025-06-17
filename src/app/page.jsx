c6ad85123408b46f2dba934a5c41a4686f16c783
Coder Nelly <codernelly34@gmail.com>
2025-06-17 11:56:59 +0100

    feat: add project files (init commit)

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio soluta,
+        amet alias iure repudiandae aut eaque quibusdam labore aperiam nam.
+        Consequatur repellat laborum sapiente veritatis, quas similique eaque
+        accusantium perferendis?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
